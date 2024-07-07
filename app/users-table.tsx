e13e4412a315e2cf185c85c404d4a983851b1e09
Father Abraham <abembecker@gmail.com>
2024-07-06 21:43:47 -0400

    chore: ui controls added for group selection / creation.

--- app/users-table.tsx
+++ app/users-table.tsx
@@ -13,6 +13,7 @@ import { useRouter } from 'next/navigation';
 import useSWR from 'swr';
 import { Spinner } from '@/components/icons';
 import { User } from '@/lib/dtos/user.dto';
+import { useState } from 'react';
 
 
 const fetcher = (...args: any[]) => fetch(`/api/users`).then(res => res.json())
@@ -31,9 +32,11 @@ export function UsersTable() {
   const router = useRouter();
   const { users, newOffset, isLoading, isError } = useUsers();
   const offset = 0;
+  const [groupAddresses, setGroupAddresses] = useState<string[]>([]);
 
   function onClick() {
-    router.replace(`/?offset=${newOffset}`);
+    console.log('create group with addresses', groupAddresses);
+    console.log('create session', groupAddresses);
   }
 
   if (isLoading) return <Spinner />
@@ -44,17 +47,16 @@ export function UsersTable() {
         <Table>
           <TableHeader>
             <TableRow>
+              <TableHead className="max-w-[50px]"></TableHead>
               <TableHead className="max-w-[150px]">Name</TableHead>
               <TableHead className="hidden md:table-cell">Email</TableHead>
               <TableHead className="hidden md:table-cell">Username</TableHead>
               <TableHead className="hidden md:table-cell">Logged In</TableHead>
-              <TableHead className="hidden md:table-cell">Actions</TableHead>
-              <TableHead></TableHead>
             </TableRow>
           </TableHeader>
           <TableBody>
             {users.map((user: User) => (
-              <UserRow key={user.walletAddress} user={user} />
+              <UserRow key={user.walletAddress} user={user} groupAddresses={groupAddresses} setGroupAddresses={setGroupAddresses}/>
             ))}
           </TableBody>
         </Table>
@@ -62,48 +64,36 @@ export function UsersTable() {
       {offset !== null && (
         <Button
           className="mt-4 w-40"
-          variant="secondary"
+          variant="default"
+          disabled={groupAddresses?.length <= 1}
           onClick={() => onClick()}
         >
-          Next Page
+          Create Session ({groupAddresses?.length || 0})
         </Button>
       )}
     </>
   );
 }
 
-function UserRow({ user }: { user: User }) {
-
-
+function UserRow({ user, groupAddresses, setGroupAddresses }: { user: User, groupAddresses: string[], setGroupAddresses: any}) {
+  function handleCheckbox(event: any) {
+    console.log(event.target.value);
+    if (event.target.checked) {
+      setGroupAddresses([...groupAddresses, event.target.value]);
+    } else {
+      setGroupAddresses(groupAddresses.filter((address) => address !== event.target));
+    }
+  }
   return (
     <TableRow>
+      <TableCell>
+        <input type="checkbox" value={user.walletAddress} onClick={handleCheckbox}/>
+      </TableCell>
       <TableCell className="font-medium">{user.name}</TableCell>
       <TableCell className="hidden md:table-cell">{user.email}</TableCell>
       <TableCell>{user.username}</TableCell>
       <TableCell>
         <span>{user.loggedIn.toString() }</span>
-      </TableCell>
-      <TableCell>
-        {/*<Button*/}
-        {/*  className="w-full"*/}
-        {/*  size="sm"*/}
-        {/*  variant="outline"*/}
-        {/*  formAction={deleteUserWithPublicId}*/}
-        {/*  disabled*/}
-        {/*>*/}
-        {/*  Delete*/}
-        {/*</Button>*/}
-        {/*<div>*/}
-        {/*  <div*/}
-        {/*    className="inline-block mb-2 ms-[calc(25%-1.25rem)] py-0.5 px-1.5 bg-blue-50 border border-blue-200 text-xs font-medium text-blue-600 rounded-lg dark:bg-blue-800/30 dark:border-blue-800 dark:text-blue-500">*/}
-        {/*    25%*/}
-        {/*  </div>*/}
-        {/*  <div className="flex w-full h-2 bg-gray-200 rounded-full overflow-hidden dark:bg-neutral-700" role="progressbar">*/}
-        {/*    <div*/}
-        {/*      className="w-3/12 flex flex-col justify-center rounded-full overflow-hidden bg-blue-600 text-xs text-white text-center whitespace-nowrap transition duration-500 dark:bg-blue-500"*/}
-        {/*      ></div>*/}
-        {/*  </div>*/}
-        {/*</div>*/}
       </TableCell>
     </TableRow>
   );
