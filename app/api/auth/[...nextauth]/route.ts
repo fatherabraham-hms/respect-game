4dc3811cb8534008cfa7420c078c2c43abfa0029
Father Abraham <abembecker@gmail.com>
2024-09-04 09:01:41 -0400

    fix: bug where progress bar was not reflecting actual percentage width.

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,2 @@
-import NextAuth from 'next-auth';
-import GitHub from 'next-auth/providers/github';
-
-const handler = NextAuth({
-  providers: [GitHub]
-})
-export { handler as GET, handler as POST };
+import { handlers } from '@/lib/auth';
+export const { GET, POST } = handlers;
