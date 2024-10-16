0a0c9fc6ba9f5bc5484a0d659f35422cbc58c9ef
Father Abraham <abembecker@gmail.com>
2024-10-15 23:14:50 -0400

    fix: build errors.

--- lib/auth.ts
+++
@@ -1,6 +0,0 @@
-const NextAuth = await import('next-auth');
-const GitHub = await import('next-auth/providers/github');
-
-export const { handlers, signIn, signOut, auth } = NextAuth.default({
-  providers: [GitHub.default]
-});
