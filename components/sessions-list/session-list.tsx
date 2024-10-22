8146ed6fac5804d59ab2e0dd4fdf6beaa29122bf
Father Abraham <abembecker@gmail.com>
2024-10-21 22:07:26 -0400

    chore: changed session table over to chakra

--- components/sessions-list/session-list.tsx
+++ components/sessions-list/session-list.tsx
@@ -1,9 +1,22 @@
 'use client';
 import { useState, useEffect } from 'react';
-import { getRecentSessionsForUserWalletAddressAction } from '@/app/actions';
+import {
+  getRecentSessionsForUserWalletAddressAction
+} from '@/app/actions';
 import { Button } from '@/components/ui/button';
 import { useRouter } from 'next/navigation';
 import { SESSION_POLLING_INTERVAL } from '../../data/constants/app_constants';
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  TableContainer,
+} from '@chakra-ui/react'
+import { Link } from '@chakra-ui/next-js';
+import { getSessionStatusLabel } from '@/lib/utils';
 
 export function SessionList() {
   const router = useRouter();
@@ -37,64 +50,34 @@ export function SessionList() {
           <div className="-m-1.5 overflow-x-auto">
             <div className="p-1.5 min-w-full inline-block align-middle">
               <div className="overflow-hidden">
-                <table className="min-w-full divide-y divide-gray-200 dark:divide-neutral-700">
-                  <thead>
-                    <tr>
-                      <th
-                        scope="col"
-                        className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase dark:text-neutral-500"
-                      >
-                        Session Number
-                      </th>
-                      <th
-                        scope="col"
-                        className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase dark:text-neutral-500"
-                      >
-                        Status
-                      </th>
-                      <th
-                        scope="col"
-                        className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase dark:text-neutral-500"
-                      >
-                        Created
-                      </th>
-                    </tr>
-                  </thead>
-                  <tbody className="divide-y divide-gray-200 dark:divide-neutral-700">
-                    {recentSessions?.length > 0 &&
-                      recentSessions.map((session) => (
-                        <tr key={session.sessionid}>
-                          <td className="px-6 py-3 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-neutral-200">
-                            <a
-                              className={
-                                'class="text-blue-600 hover:text-blue-500 decoration-2 hover:underline focus:outline-none focus:underline opacity-90'
-                              }
-                              href={`/play/${session.sessionid}`}
-                            >
-                              Session #{session.sessionid}
-                            </a>
-                            &#x2003;
-                            {session.sessionStatus === 1 && (
-                              <Button
-                                className={
-                                  'bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-2 text-sm rounded'
-                                }
-                                onClick={() => joinSession(session.sessionid)}
-                              >
-                                Join
-                              </Button>
-                            )}
-                          </td>
-                          <td className="px-6 py-3 whitespace-nowrap text-sm text-gray-800 dark:text-neutral-200">
-                            {session.sessionStatus}
-                          </td>
-                          <td className="px-6 py-3 whitespace-nowrap text-sm text-gray-800 dark:text-neutral-200">
-                            {session.updated.toLocaleDateString()}
-                          </td>
-                        </tr>
-                      ))}
-                  </tbody>
-                </table>
+                <TableContainer>
+                  <Table>
+                    <Thead>
+                      <Tr>
+                        <Th>Session Number</Th>
+                        <Th>Status</Th>
+                        <Th>Created</Th>
+                      </Tr>
+                    </Thead>
+                    <Tbody>
+                      {recentSessions?.length > 0 &&
+                        recentSessions.map((session) => (
+                          <Tr key={session.sessionid}>
+                            <Td>
+                              <Link href={`/play/${session.sessionid}`}>Session #{session.sessionid}</ Link>
+                              &#x2003;
+                              {session.sessionStatus === 1 && (
+                                <Button onClick={() => joinSession(session.sessionid)}>Join</Button>
+                              )}
+                            </Td>
+                            <Td>{getSessionStatusLabel(session?.sessionStatus)}</Td>
+                            <Td>{session.updated.toLocaleDateString()}</Td>
+                          </Tr>
+                        ))
+                      }
+                    </Tbody>
+                  </Table>
+                </TableContainer>
                 {!recentSessions ||
                   (recentSessions.length === 0 && <h2>No Sessions to show</h2>)}
               </div>
